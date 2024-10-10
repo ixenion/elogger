908b7be578d897b04f589da3a869046a3f5a532a
ixenion <stridjj@gmail.com>
2024-10-10 10:10:10 +0300

    Added feature to get module_name bu str.

--- src/elogger/main.py
+++ src/elogger/main.py
@@ -18,7 +18,10 @@ from loguru._handler    import Handler
 # Local imports #
 
 from elogger.utils.datastructures    import LOG_FOLDER, Singleton, Const
-from elogger.utils.exceptions        import LoggerAttributeError
+from elogger.utils.exceptions        import (
+        LoggerAttributeError,
+        ModuleNotFoundException,
+        )
 
 
 
@@ -227,3 +230,14 @@ class Logger(Singleton, dict):
         self[module_name] = logger
 
         return logger
+
+
+    def get_logger_unit(self, module_name:str) -> LoggerUnit:
+        """
+            Return logger module by string.
+            Example: logger.get_logger_unit(f"{some_module_name}").info(f"I'm here")
+        """
+        
+        if module_name in self:
+            return self[module_name]
+        raise ModuleNotFoundException(module_name)
--- src/elogger/utils/exceptions.py
+++ src/elogger/utils/exceptions.py
@@ -27,3 +27,13 @@ class LoggerAttributeError(AttributeError):
     def __init__(self, attr_name:str) -> None:
         self.message = f"Logger has no attribute '{attr_name}'. Create it first."
         super().__init__(self.message)
+
+
+class ModuleNotFoundException(Exception):
+    """
+        There is no module you are calling...
+    """
+
+    def __init__(self, module_name:str) -> None:
+        self.message = f"There is no module you are calling: {module_name}"
+        super().__init__(self.message)
